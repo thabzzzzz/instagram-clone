6b179b9d04512712e3f104559a9d47f0cdfae1bb
Thabiso <tmosenohi99@gmail.com>
2024-08-13 16:24:46 +0200

    no message

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -1,8 +1,24 @@
 import { ref } from 'vue'
 import { defineStore } from 'pinia'
 
+interface credentials {
+  email: string;
+  password: string;
+  username: string;
+}
+
 export const useUserStore = defineStore('users', () => {
  const user = ref(null);
+
+ const validateEmail = (email) => {
+  return String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+};
+
+ const errorMessage= ref('');
 
  const handleLogin = ()=>{
 
@@ -10,12 +26,19 @@ export const useUserStore = defineStore('users', () => {
 
  const handleSignup = (credentials)=>{
   const {email, password, username}=credentials;
-  const errorMessage= ref('');
+ 
 
   if (password.length<6){
-    return errorMessage.value='Password length is too short'
+    return errorMessage.value='Password length must be atleast 6 characters'
   }
 
+  if (username.length<4){
+    return errorMessage.value='Username length must be atleast 4 characters'
+  }
+
+  if (!validateEmail(email)){
+    errorMessage.value='Email is invalid'
+  }
  }
 
  const handleLogout = ()=>{
@@ -26,5 +49,5 @@ export const useUserStore = defineStore('users', () => {
   
  }
 
-  return { handleLogin, handleLogout, handleSignup, getUser }
+  return { handleLogin, handleLogout, handleSignup, getUser, errorMessage }
 })
